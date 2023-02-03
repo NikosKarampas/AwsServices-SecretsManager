ba9332c9d680fac1383a570c40ff1f149f770fe0
Nikos Karampas <nikos.karampas@hotmail.com>
2023-02-03 16:21:11 +0200

    ListSecretVersionIdsAsync

--- SecretsManager.App/Program.cs
+++ SecretsManager.App/Program.cs
@@ -4,20 +4,30 @@ using Amazon.SecretsManager.Model;
 
 var secretsManagerClient = new AmazonSecretsManagerClient();
 
+var listSecretVersionsRequest = new ListSecretVersionIdsRequest
+{
+    SecretId = "ApiKey",
+    IncludeDeprecated = true
+};
+
+var versionResponse = await secretsManagerClient.ListSecretVersionIdsAsync(listSecretVersionsRequest);
+
 var request = new GetSecretValueRequest
 {
-    SecretId = "ApiKey"
+    SecretId = "ApiKey",
+    //VersionStage = "AWSCURRENT"
+    VersionId = "be99f6e2-7ccb-4dd4-8d8c-aa842ac93e41"
 };
 
 var response = await secretsManagerClient.GetSecretValueAsync(request);
 
 Console.WriteLine(response.SecretString);
 
-var describeSecretRequest = new DescribeSecretRequest
-{
-    SecretId = "ApiKey"
-};
+//var describeSecretRequest = new DescribeSecretRequest
+//{
+//    SecretId = "ApiKey"
+//};
 
-var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeSecretRequest);
+//var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeSecretRequest);
 
-Console.WriteLine();
+//Console.WriteLine();
